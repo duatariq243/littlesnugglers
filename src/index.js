d774fc9630954b907f47f921def6cd8c63583cbf
Dua Tariq <duatariq243@gmail.com>
2025-07-14 16:49:46 -0400

    Fix homepage and deployment config

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import ReactDOM from 'react-dom/client';
 import App from './App';
+function AppWrapper() {
+  return (
+    <BrowserRouter basename="/littlesnugglers">
+      <App />
+    </BrowserRouter>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="/littlesnugglers">
-    <App />
-  </BrowserRouter>
+export default AppWrapper;
 
-);
